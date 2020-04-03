7bfa424ced8c1a648ca0846f7891121380b1cdd9
harshitdholakia7 <62429097+harshitdholakia7@users.noreply.github.com>
2020-04-03 12:05:10 +0530

    Added TandCFragment

--- app/src/main/java/com/example/pedocon/fragment/TandCFragment.java
+++ app/src/main/java/com/example/pedocon/fragment/TandCFragment.java
@@ -1,5 +1,6 @@
 package com.example.pedocon.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,23 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.pedocon.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class TandCFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class TandCFragment extends Fragment
+{
+    @BindView(R.id.webview)WebView webView;
     public TandCFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_t_and_c, container, false);
+                             Bundle savedInstanceState)
+    {
+        View view=inflater.inflate(R.layout.fragment_t_and_c, container, false);
+        ButterKnife.bind(this,view);
+
+        webView.loadUrl("http://pedocon2020.com/TermsAndConditions.aspx");
+
+        return view;
     }
 }
